cb8e8bf97431097b6350903731140920db301cb6
xiaoqiu123456 <2546345963@qq.com>
2022-04-09 17:34:09 +0800

    sub服务

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style/reset.css'
 import './style/common.css'
+import './assets/font_icon/iconfont.css'
 
 createApp(App).mount('#app')
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,6 +4,27 @@ import Nav from './Nav.vue'
 import Footer from './Footer.vue'
 import Menu from './Menu.vue'
 import MenuBanner from './MenuBanner.vue'
+const subs = [
+  {
+    icon: 'icon-naozhong',
+    text: '保障服务'
+  },{
+    icon: 'icon-fangdichanjianzhu',
+    text: '企业团购'
+  },{
+    icon: 'icon-fmabeifen2',
+    text: 'F码通道'
+  },{
+    icon: 'icon-cunchuneicunka',
+    text: '米粉卡'
+  },{
+    icon: 'icon-qian',
+    text: '以旧换新'
+  },{
+    icon: 'icon-huafeichongzhi',
+    text: '花费充值'
+  },
+]
 </script>
 
 <template>
@@ -21,8 +42,36 @@ import MenuBanner from './MenuBanner.vue'
       </div>
 
       <!-- 服务 -->
-      <div class="sub">
-        <div class="container">服务</div>
+      <div class="sub container">
+        <div class="sub-left">
+          <ul>
+            <li v-for="(sub, i) in subs" :key="i">
+              <a href="#">
+                <span :class="['iconfont', sub.icon]"></span>
+                <p>{{sub.text}}</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="sub-right">
+          <ul>
+              <li style="margin-left: 0">
+                <a href="#">
+                  <img src="#" alt="">
+                </a>
+              </li>
+            <li>
+              <a href="#">
+                <img src="#" alt="">
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="#" alt="">
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </main>
 
@@ -56,6 +105,51 @@ import MenuBanner from './MenuBanner.vue'
 }
 
 .sub {
+  display: flex;
   height: 170px;
+  margin-top: 14px;
+}
+.sub-left{
+  width: 234px;
+  height: 170px;
+}
+.sub-left ul{
+  margin: 0;
+  padding: 3px;
+  list-style-type: none;
+  font-size: 12px;
+  text-align: center;
+  background: #5f5750;
+  overflow: hidden;
+}
+.sub-left ul li{
+  position: relative;
+  float: left;
+  width: 76px;
+  height: 82px;
+  padding: 0 3px;
+}
+.sub-left a{
+  display: block;
+  padding-top: 18px;
+  text-overflow: ellipsis;
+  color: #fff;
+  opacity: .7;
+  -webkit-transition: opacity .2s;
+  transition: opacity .2s;
+}
+.sub-right{
+  width: 978px;
+  height: 170px;
+  margin-left: 14px;
+}
+.sub-right ul{
+  display: flex;
+}
+.sub-right li{
+  width: 316px;
+  height: 170px;
+  margin-left: 15px;
+  background-color: #007aff;
 }
 </style>
